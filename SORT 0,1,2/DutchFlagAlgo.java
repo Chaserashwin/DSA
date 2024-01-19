0f2cd72c0f1f99ec3f4789e651bb6db3d9038e5d
Ashwin Jaiswal <jaiswalashwin398@gmail.com>
2024-01-19 23:28:09 +0530

    leetcode 0075

---
+++ SORT 0,1,2/DutchFlagAlgo.java
@@ -0,0 +1,35 @@
+/*
+
+Most optimal way to sort the array containing 0|1|2 is by using the ["Dutch Flag Algorithm"]
+
+Explaination of this Algorithm:-
+we use 3 pointers here to use this Algo...
+
+        [0...low-1]    -> 0  (extreamly left)
+        [low...mid-1]  -> 1
+        [high+1...n-1] -> 2  (extreamly right)
+
+        |-----------------------------------------------------------------|
+        |                                                                 |
+        |        0            low          mid          high+1            |
+        |        |             |            |             |               |
+        |        |             |            |             |               |
+        |       [000000000]   [11111111]   [120210210]   [22222222]       |
+        |                |            |             |            |        |
+        |                |            |             |            |        |
+        |              low-1        mid-1         high          n-1       |
+        |                                                                 |
+        |         (sorted)     (sorted)     (unsorted)    (sorted)        |
+        |                                                                 |
+        |-----------------------------------------------------------------|
+
+        Now to sort the unsorted array we will be using these pointers like:
+        -> low and mid is pointed at the start of the array
+        -> high is pointed at the end of the array
+        So, to sort an array
+                    if(a[mid] == 0)  { swap(a[low], a[mid]), low++, mid++ }
+                    if(a[mid] == 1)  { mid++ }
+                    if(a[mid] == 2)  { swap(a[high], a[mid]), high-- }
+
+
+*/
